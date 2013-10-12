841cd58871d16cd07f6075c96d0cae58fc4bac06
Harry van Wier <harryvanwier@gmail.com>
2013-10-12 22:48:43 +0200

    added more blocks

--- blocks.cpp
+++ blocks.cpp
@@ -9,6 +9,7 @@ std::map<unsigned short, Block> Blocks::blockRegister = std::map<unsigned short,
 namespace BlockTypes {
 	enum Enum {
 		Air, Stone, Grass, Dirt, Bedrock, Water, Lava, Sand, Gravel, GoldOre, IronOre, CoalOre, DiamondOre, Wood, Leaves, LazuliOre, SandStone, Snow, Ice, Cactus, Tree, Clay, Obsidian, Green_SugarCane, Green_Grass, Green_DeadBush, Green_FlowerYellow, Green_FlowerRed, RedstoneOre
+		//   got    got    got   got      got    got   got   got     got      got      got      got         got   got     needtex    needtex    got   got  got     got   got   got       got              needtex      needtex         got                 got              got
 	};
 }
 
@@ -59,6 +60,15 @@ void Blocks::initBlocks()
 	SETTEXTUREAT(water.westTexCoords, 15, 13);
 	blockRegister[BlockTypes::Water] = water;
 	
+	Block lava;
+	SETTEXTUREAT(water.topTexCoords, 15, 15);
+	SETTEXTUREAT(water.bottomTexCoords, 15, 15);
+	SETTEXTUREAT(water.northTexCoords, 15, 15);
+	SETTEXTUREAT(water.eastTexCoords, 15, 15);
+	SETTEXTUREAT(water.southTexCoords, 15, 15);
+	SETTEXTUREAT(water.westTexCoords, 15, 15);
+	blockRegister[BlockTypes::Lava] = lava;
+	
 	Block tree;
 	SETTEXTUREAT(tree.topTexCoords, 5, 1);
 	SETTEXTUREAT(tree.bottomTexCoords, 5, 1);
@@ -76,6 +86,15 @@ void Blocks::initBlocks()
 	SETTEXTUREAT(leaves.southTexCoords, 5, 3);
 	SETTEXTUREAT(leaves.westTexCoords, 5, 3);
 	blockRegister[BlockTypes::Leaves] = leaves;
+	
+	Block wood;
+	SETTEXTUREAT(wood.topTexCoords, 4, 0);
+	SETTEXTUREAT(wood.bottomTexCoords, 4, 0);
+	SETTEXTUREAT(wood.northTexCoords, 4, 0);
+	SETTEXTUREAT(wood.eastTexCoords, 4, 0);
+	SETTEXTUREAT(wood.southTexCoords, 4, 0);
+	SETTEXTUREAT(wood.westTexCoords, 4, 0);
+	blockRegister[BlockTypes::Wood] = wood;
 	
 	Block diamondOre;
 	SETTEXTUREAT(diamondOre.topTexCoords, 2, 3);
@@ -112,4 +131,105 @@ void Blocks::initBlocks()
 	SETTEXTUREAT(ice.southTexCoords, 3, 4);
 	SETTEXTUREAT(ice.westTexCoords, 3, 4);
 	blockRegister[BlockTypes::Ice] = ice;
+	
+	Block gravel;
+	SETTEXTUREAT(gravel.topTexCoords, 3, 1);
+	SETTEXTUREAT(gravel.bottomTexCoords, 3, 1);
+	SETTEXTUREAT(gravel.northTexCoords, 3, 1);
+	SETTEXTUREAT(gravel.eastTexCoords, 3, 1);
+	SETTEXTUREAT(gravel.southTexCoords, 3, 1);
+	SETTEXTUREAT(gravel.westTexCoords, 3, 1);
+	blockRegister[BlockTypes::Gravel] = gravel;
+	
+	Block goldOre;
+	SETTEXTUREAT(goldOre.topTexCoords, 0, 2);
+	SETTEXTUREAT(goldOre.bottomTexCoords, 0, 2);
+	SETTEXTUREAT(goldOre.northTexCoords, 0, 2);
+	SETTEXTUREAT(goldOre.eastTexCoords, 0, 2);
+	SETTEXTUREAT(goldOre.southTexCoords, 0, 2);
+	SETTEXTUREAT(goldOre.westTexCoords, 0, 2);
+	blockRegister[BlockTypes::GoldOre] = goldOre;
+	
+	Block ironOre;
+	SETTEXTUREAT(ironOre.topTexCoords, 1, 2);
+	SETTEXTUREAT(ironOre.bottomTexCoords, 1, 2);
+	SETTEXTUREAT(ironOre.northTexCoords, 1, 2);
+	SETTEXTUREAT(ironOre.eastTexCoords, 1, 2);
+	SETTEXTUREAT(ironOre.southTexCoords, 1, 2);
+	SETTEXTUREAT(ironOre.westTexCoords, 1, 2);
+	blockRegister[BlockTypes::IronOre] = ironOre;
+	
+	Block coalOre;
+	SETTEXTUREAT(coalOre.topTexCoords, 2, 2);
+	SETTEXTUREAT(coalOre.bottomTexCoords, 2, 2);
+	SETTEXTUREAT(coalOre.northTexCoords, 2, 2);
+	SETTEXTUREAT(coalOre.eastTexCoords, 2, 2);
+	SETTEXTUREAT(coalOre.southTexCoords, 2, 2);
+	SETTEXTUREAT(coalOre.westTexCoords, 2, 2);
+	blockRegister[BlockTypes::CoalOre] = coalOre;
+	
+	Block redstoneOre;
+	SETTEXTUREAT(redstoneOre.topTexCoords, 3, 3);
+	SETTEXTUREAT(redstoneOre.bottomTexCoords, 3, 3);
+	SETTEXTUREAT(redstoneOre.northTexCoords, 3, 3);
+	SETTEXTUREAT(redstoneOre.eastTexCoords, 3, 3);
+	SETTEXTUREAT(redstoneOre.southTexCoords, 3, 3);
+	SETTEXTUREAT(redstoneOre.westTexCoords, 3, 3);
+	blockRegister[BlockTypes::RedstoneOre] = redstoneOre;
+	
+	Block cactus;
+	SETTEXTUREAT(cactus.topTexCoords, 5, 4);
+	SETTEXTUREAT(cactus.bottomTexCoords, 7, 4);
+	SETTEXTUREAT(cactus.northTexCoords, 6, 4);
+	SETTEXTUREAT(cactus.eastTexCoords, 6, 4);
+	SETTEXTUREAT(cactus.southTexCoords, 6, 4);
+	SETTEXTUREAT(cactus.westTexCoords, 6, 4);
+	blockRegister[BlockTypes::Cactus] = cactus;
+	
+	Block clay;
+	SETTEXTUREAT(clay.topTexCoords, 8, 4);
+	SETTEXTUREAT(clay.bottomTexCoords, 8, 4);
+	SETTEXTUREAT(clay.northTexCoords, 8, 4);
+	SETTEXTUREAT(clay.eastTexCoords, 8, 4);
+	SETTEXTUREAT(clay.southTexCoords, 8, 4);
+	SETTEXTUREAT(clay.westTexCoords, 8, 4);
+	blockRegister[BlockTypes::Clay] = clay;
+	
+	Block obsidian;
+	SETTEXTUREAT(obsidian.topTexCoords, 5, 2);
+	SETTEXTUREAT(obsidian.bottomTexCoords, 5, 2);
+	SETTEXTUREAT(obsidian.northTexCoords, 5, 2);
+	SETTEXTUREAT(obsidian.eastTexCoords, 5, 2);
+	SETTEXTUREAT(obsidian.southTexCoords, 5, 2);
+	SETTEXTUREAT(obsidian.westTexCoords, 5, 2);
+	blockRegister[BlockTypes::Obsidian] = obsidian;
+	
+	Block sugarCane;
+	SETTEXTUREAT(sugarCane.topTexCoords, 9, 4);
+	SETTEXTUREAT(sugarCane.bottomTexCoords, 9, 4);
+	SETTEXTUREAT(sugarCane.northTexCoords, 9, 4);
+	SETTEXTUREAT(sugarCane.eastTexCoords, 9, 4);
+	SETTEXTUREAT(sugarCane.southTexCoords, 9, 4);
+	SETTEXTUREAT(sugarCane.westTexCoords, 9, 4);
+	blockRegister[BlockTypes::Green_SugarCane] = sugarCane;
+	
+	Block flower;
+	SETTEXTUREAT(flower.topTexCoords, 13, 0);
+	SETTEXTUREAT(flower.bottomTexCoords, 13, 0);
+	SETTEXTUREAT(flower.northTexCoords, 13, 0);
+	SETTEXTUREAT(flower.eastTexCoords, 13, 0);
+	SETTEXTUREAT(flower.southTexCoords, 13, 0);
+	SETTEXTUREAT(flower.westTexCoords, 13, 0);
+	blockRegister[BlockTypes::Green_FlowerYellow] = flower;
+	
+	Block rose;
+	SETTEXTUREAT(rose.topTexCoords, 12, 0);
+	SETTEXTUREAT(rose.bottomTexCoords, 12, 0);
+	SETTEXTUREAT(rose.northTexCoords, 12, 0);
+	SETTEXTUREAT(rose.eastTexCoords, 12, 0);
+	SETTEXTUREAT(rose.southTexCoords, 12, 0);
+	SETTEXTUREAT(rose.westTexCoords, 12, 0);
+	blockRegister[BlockTypes::Green_FlowerRed] = rose;
+	
+	
 }
