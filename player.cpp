f6543ad3e538717338434d55ce831097f44b560b
Harry van Wier <harryvanwier@gmail.com>
2013-10-28 16:25:13 +0100

    player now using Entity system for moving

--- player.cpp
+++ player.cpp
@@ -6,7 +6,7 @@
 Player::Player()
 :playerId(0)
 {
-	bbox = BoundingBox();
+	bbox = BoundingBox(-0.3, -0.3, 0.0, 0.3, 0.3, 1.8);
 }
 
 Player::~Player()
@@ -18,30 +18,42 @@ void Player::Update()
 {	
 	GLFWwindow* window = Globals::getGameInstance().window;
 	
+	Vector3 motion(0);
+	
 	float speed = 1.0f;
 	//forward/backward motion TODO: move to player class
 	if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 	{
-		camera.AddX(sin(camera.GetPitch()) * cos(camera.GetYaw()) * speed);
-		camera.AddY(sin(camera.GetPitch()) * sin(camera.GetYaw()) * speed);
-		camera.AddZ(cos(camera.GetPitch()) * speed);
+		motion += Vector3(
+			sin(camera.GetPitch()) * cos(camera.GetYaw()) * speed,
+			sin(camera.GetPitch()) * sin(camera.GetYaw()) * speed,
+			cos(camera.GetPitch()) * speed
+		);
 	}
 	if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
 	{
-		camera.AddX(-sin(camera.GetPitch()) * cos(camera.GetYaw()) * speed);
-		camera.AddY(-sin(camera.GetPitch()) * sin(camera.GetYaw()) * speed);
-		camera.AddZ(-cos(camera.GetPitch()) * speed);
+		motion += Vector3(
+			-sin(camera.GetPitch()) * cos(camera.GetYaw()) * speed,
+			-sin(camera.GetPitch()) * sin(camera.GetYaw()) * speed,
+			-cos(camera.GetPitch()) * speed
+		);
 	}
 	//left/right motion
 	if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
 	{
-		camera.AddX(cos(float(camera.GetYaw() + (M_PI / 180) * -90)) * speed);
-		camera.AddY(sin(float(camera.GetYaw() + (M_PI / 180) * -90)) * speed);
+		motion += Vector3(
+			cos(float(camera.GetYaw() + (M_PI / 180) * -90)) * speed,
+			sin(float(camera.GetYaw() + (M_PI / 180) * -90)) * speed,
+			0
+		);
 	}
 	if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 	{
-		camera.AddX(cos(float(camera.GetYaw() + (M_PI / 180) * 90)) * speed);
-		camera.AddY(sin(float(camera.GetYaw() + (M_PI / 180) * 90)) * speed);
+		motion += Vector3(
+			cos(float(camera.GetYaw() + (M_PI / 180) * 90)) * speed,
+			sin(float(camera.GetYaw() + (M_PI / 180) * 90)) * speed,
+			0
+		);
 	}
 	
 	double mouseX, mouseY;
@@ -60,7 +72,11 @@ void Player::Update()
 	glfwSetCursorPos(window, 1024 / 2, 768 / 2);
 	glfwGetCursorPos(window, &prevMouseX, &prevMouseY);
 	
+	this->vel = motion;
+	
 	Entity::Update();
+	
+	camera.SetPos(this->pos.x, this->pos.y, this->pos.z);
 }
 
 void Player::Draw()
