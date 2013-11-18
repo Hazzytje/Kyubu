82fc9a1171a1b8073c3ff321cd75e1d7b26d17b5
Harry van Wier <harryvanwier@gmail.com>
2013-11-18 15:10:20 +0100

    starting work on textrenderer class

---
+++ textrenderer.cpp
@@ -0,0 +1,21 @@
+#include "textrenderer.h"
+
+TextRenderer::TextRenderer()
+{
+	
+}
+
+TextRenderer::~TextRenderer()
+{
+	
+}
+
+void TextRenderer::Render()
+{
+	
+}
+
+void TextRenderer::AddText(std::string text, int x, int y)
+{
+	
+}
