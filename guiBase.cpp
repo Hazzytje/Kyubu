b39a40688b4a04c9d2a5f9cc63286b567f182d88
Harry van Wier <harryvanwier@gmail.com>
2014-02-16 20:12:03 +0100

    Revert "removed base class function implemenatations"
    This reverts commit 450bc5ba77559116e94854c964ee7d037a4370ce.

--- guiBase.cpp
+++ guiBase.cpp
@@ -11,4 +11,66 @@ namespace KyubuGui
 	{
 
 	}
+
+	void GuiBase::Update()
+	{
+
+	}
+
+	void GuiBase::OnLeftMousePress(int x, int y)
+	{
+
+	}
+	void GuiBase::OnRightMousePress(int x, int y)
+	{
+
+	}
+	void GuiBase::OnMiddleMousePress(int x, int y)
+	{
+
+	}
+
+	void GuiBase::OnLeftMouseRelease(int x, int y)
+	{
+
+	}
+	void GuiBase::OnRightMouseRelease(int x, int y)
+	{
+
+	}
+	void GuiBase::OnMiddleMouseRelease(int x, int y)
+	{
+
+	}
+
+	void GuiBase::OnLeftMouseClick(int x, int y)
+	{
+
+	}
+	void GuiBase::OnRightMouseClick(int x, int y)
+	{
+
+	}
+	void GuiBase::OnMiddleMouseClick(int x, int y)
+	{
+
+	}
+
+	void GuiBase::OnScrollUp()
+	{
+
+	}
+	void GuiBase::OnScrollDown()
+	{
+
+	}
+
+	void GuiBase::OnKeyDown()
+	{
+
+	}
+	void GuiBase::OnKeyUp()
+	{
+
+	}
 }
